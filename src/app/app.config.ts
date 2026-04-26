7f1dbff48624387ad2911ba5e8935a6f3ed78aa8
joseguambo1994 <66379605+joseguambo1994@users.noreply.github.com>
2026-04-26 18:10:56 -0500

    feat: Add pwa. Remove unnesary ssr making cloudflare deployment fail.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withFetch()), 
+  providers: [
+    provideHttpClient(withFetch()),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
