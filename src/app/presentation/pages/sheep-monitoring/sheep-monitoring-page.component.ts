e172288b97c494cdf956cf2ff909abd3b00fe927
joseguambo1994 <66379605+joseguambo1994@users.noreply.github.com>
2026-04-26 21:46:22 -0500

    chore: Add video loader.

--- src/app/presentation/pages/sheep-monitoring/sheep-monitoring-page.component.ts
+++ src/app/presentation/pages/sheep-monitoring/sheep-monitoring-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { SheepMonitoringFacade } from '../../../application/services/sheep-monitoring.facade';
 import { SheepCounterItem, SheepCounterSnapshot } from '../../../domain/models/sheep-monitoring.models';
 
@@ -9,14 +9,28 @@ import { SheepCounterItem, SheepCounterSnapshot } from '../../../domain/models/s
 })
 export class SheepMonitoringPageComponent {
   private readonly monitoringFacade = inject(SheepMonitoringFacade);
+  private monitoringVideo?: HTMLVideoElement;
+  private hasLoadedInitially = false;
 
   protected readonly snapshot = signal<SheepCounterSnapshot>(
     this.monitoringFacade.buildInitialSnapshot(),
   );
   protected readonly videoSource = this.monitoringFacade.getVideoSource();
-  protected readonly posterSource = this.monitoringFacade.getPosterSource();
+  protected readonly isVideoLoading = signal(true);
 
   protected handleMetadata(video: HTMLVideoElement): void {
+    this.monitoringVideo = video;
+    this.startLoopedPlayback(video);
+    this.refreshSnapshot(video);
+  }
+
+  protected handleCanPlay(video: HTMLVideoElement): void {
+    this.monitoringVideo = video;
+    if (!this.hasLoadedInitially) {
+      this.hasLoadedInitially = true;
+      this.isVideoLoading.set(false);
+    }
+    this.startLoopedPlayback(video);
     this.refreshSnapshot(video);
   }
 
@@ -26,6 +40,20 @@ export class SheepMonitoringPageComponent {
 
   protected trackCounter(_: number, counter: SheepCounterItem): string {
     return counter.id;
+  }
+
+  @HostListener('document:visibilitychange')
+  protected handleVisibilityChange(): void {
+    if (document.visibilityState === 'visible' && this.monitoringVideo) {
+      this.startLoopedPlayback(this.monitoringVideo);
+    }
+  }
+
+  private startLoopedPlayback(video: HTMLVideoElement): void {
+    video.muted = true;
+    video.defaultMuted = true;
+    video.loop = true;
+    void video.play().catch(() => undefined);
   }
 
   private refreshSnapshot(video: HTMLVideoElement): void {
